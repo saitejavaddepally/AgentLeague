a3c9338a54d514645c50296c42c193a474c77bdc
saitejavaddepally <saitejavaddepally11@gmail.com>
2022-03-22 14:26:01 +0530

    added screens and custom line under text

--- lib/components/custom_line_under_text.dart
+++ lib/components/custom_line_under_text.dart
@@ -4,16 +4,21 @@
 
 import 'package:flutter/material.dart';
 
+
 class CustomLineUnderText{
+  late double width;
+  late Color color;
+
+  CustomLineUnderText({this.width = 100, this.color = Colors.lightGreen});
 
   use() {
     return Center(
       child: Container(
         height: 10,
-        width: 100,
+        width: 200,
         decoration: BoxDecoration(
           border: Border.all(),
-          color: Colors.orange,
+          color: color,
           borderRadius: const BorderRadius.all(Radius.circular(30)),
         ),
       ),
